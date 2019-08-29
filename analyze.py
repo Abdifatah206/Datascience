3d261a11dc365a181ce28dae7bade6135194bf05
Abdifatah.206 <shire2567@gmail.com>
2019-08-28 23:50:49 -0700

    python dataframe created for data analayze

--- analyze.py
+++ analyze.py
@@ -4,4 +4,13 @@ Created on Wed Aug 28 23:30:27 2019
 
 @author: Shire
 """
+import pandas as pd
 
+data = {
+    'apples': [3, 2, 0, 1], 
+    'oranges': [0, 3, 7, 2]
+}
+purchase = pd.DataFrame(data, index = ['Abdi', 'casha', 'jama', 'ali'])
+#print(purchase)
+
+print(purchase.loc['Abdi'])
